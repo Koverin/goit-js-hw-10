96c793f69162870ad78df9d347070c7ff708cf1a
Koverin <110396064+Koverin@users.noreply.github.com>
2023-01-16 18:48:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+list.style.padding = '0';
+
+const info = document.querySelector('.country-info');
+
+function clearData(output) {
+  output.innerHTML = '';
+}
+
+function seachCountry(evt) {
+  const name = input.value.trim();
+
+  fetchCountries(name)
+    .then(data => {
+      createMarkup(data);
+    })
+    .catch(error => {
+      if (name !== '') {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
+    });
+  evt.preventDefault();
+}
+
+function createMarkup(array) {
+  if (array.length > 10) {
+    clearData(list);
+    clearData(info);
+
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (array.length > 1 && array.length <= 10) {
+    clearData(list);
+    clearData(info);
+
+    const createListMarkupStart = array
+      .map(
+        ({ name, flags }) =>
+          `<li style="display: flex; gap: 10px; align-items: center;">
+    <img src="${flags.svg}" alt="${name.official}" width="60" height="30">
+    <p>${name.official}</p>
+    </li>`
+      )
+      .join('');
+
+    list.insertAdjacentHTML('beforeend', createListMarkupStart);
+  } else {
+    clearData(list);
+    clearData(info);
+
+    const createListMarkupEnd = array
+      .map(
+        ({ name, flags, capital, population, languages }) =>
+          `
+    <div>
+            <img 
+                src="${flags.svg}" 
+                alt="${name.official}" 
+                width="60"
+                height="30">
+        <div>    
+            <h2>${name.official}</h2>
+            <p><b>Capital:&nbsp</b>${capital}</p>
+            <p><b>Population:&nbsp</b>${population}</p>  
+            <p><b>languages:&nbsp</b>${Object.values(languages)}</p>
+    </div>
+    </div>`
+      )
+      .join('');
+
+    list.insertAdjacentHTML('beforeend', createListMarkupEnd);
+  }
+}
+
+input.addEventListener('input', debounce(seachCountry, DEBOUNCE_DELAY));
